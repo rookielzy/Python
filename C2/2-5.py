7228174f6e6bd329991a25586244242f742b25cb
rookielzy <slimjetlzy@gmail.com>
2016-10-23 09:46:54 +0800

    Python

---
+++ C2/2-5.py
@@ -0,0 +1,9 @@
+#! whie loop
+i = 0
+while (i <= 10):
+    print i,
+    i = i + 1
+
+for j in range(0,11):
+    print j,
+    
---
+++ C2/2-7.py
@@ -0,0 +1,12 @@
+# while loop
+
+i = raw_input(">: ")
+j = 0
+while (j < len(i)):
+    print i[j],
+    j = j + 1
+
+# for loop
+
+for j in range(len(i)):
+    print j, i[j]
